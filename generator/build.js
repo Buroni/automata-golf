ce34df06f5b85cdcdac46d09ca560d22e6abd6e6
Jake Browning <jake.reid.browning@gmail.com>
2022-09-22 18:46:53 +0100

    add generator

---
+++ generator/build.js
@@ -0,0 +1,95 @@
+const { parser } = require("../parser/grammar.js");
+const { serialize } = require("../generator/serialize.js");
+const { BuildError } = require("../utils.js");
+const { isMetaProperty } = require("../utils");
+const fs = require("fs");
+
+function filterMetaProperties(machine) {
+    delete machine._serialize;
+    for (const state in machine.transitions) {
+        for (const transitionName in machine.transitions[state]) {
+            if (isMetaProperty(transitionName)) {
+                delete machine.transitions[state][transitionName];
+            }
+        }
+    }
+    return machine;
+}
+
+function build(src, { emitFile, strictActions } = {}) {
+    const unpackedRules = parser.parse(src);
+    const { initial, transitions } = unpackedRules;
+    const ret = {};
+
+    const machine = {
+        stack: ["Z"],
+        state: initial,
+        transitions,
+        dispatch: function(actionName) {
+            const stackValue = this.stack.pop();
+            let transitionActionName;
+            for (const key in this.transitions[this.state]) {
+                if (key.startsWith(`${actionName},`)) {
+                    const [, stackTransition] = key.split(",");
+                    if (!stackTransition || stackTransition === stackValue) {
+                        transitionActionName = key;
+                        break;
+                    }
+                }
+            }
+
+            try {
+                const action = this.transitions[this.state][transitionActionName];
+                action.call(this);
+            } catch(e) {
+                if (strictActions) {
+                    throw BuildError(`Invalid action: '${actionName}' from state '${this.state}'`);
+                }
+            }
+        },
+        consume: function(states, f = token => token) {
+            for (const s of states) {
+                const transitionName = f(s);
+                this.dispatch(transitionName);
+            }
+            return this;
+        },
+        stackIsInitial: function({ reset } = {reset: false}) {
+            const isInitial = this.stack.length === 1 && this.stack[0] === "Z";
+            if (reset) {
+                this.reset();
+            }
+            return isInitial;
+        },
+        reset: function() {
+            this.stack = ["Z"];
+            this.state = initial;
+            return this;
+        },
+        _serialize: function(f) {
+            const serialized = serialize.call(this, initial, strictActions, transitions);
+            if (f) {
+                fs.writeFile(f, serialized, (err) => {
+                    if (err) {
+                        throw BuildError(`Could not write to file: ${err}`);
+                    }
+                });
+            }
+            return serialized;
+        }
+    };
+
+    if (emitFile && typeof emitFile === "string") {
+        ret.src = machine._serialize(emitFile)
+    } else if (emitFile) {
+        ret.src = machine._serialize();
+    }
+    ret.machine = filterMetaProperties(machine);
+    return ret;
+}
+
+function inline(strings, ...values) {
+    return build(String.raw({ raw: strings }, ...values)).machine;
+}
+
+module.exports = { build, inline };
---
+++ generator/serialize.js
@@ -0,0 +1,60 @@
+const { isMetaProperty } = require("../utils");
+
+function impureTransitionSrc(transitionName, nextState, stackVal) {
+    return `'${transitionName}': 
+        function() { 
+            this.state = '${nextState}'; 
+            this.stack.push(${stackVal.split(",").map(el => `'${el}'`).join(",")});
+         }`
+}
+
+function pureTransitionSrc(transitionName, nextState) {
+    return `'${transitionName}': 
+        function() { 
+            this.state = '${nextState}'; 
+        }`;
+}
+
+function makeTransitionSrc(transitionName, nextState, stackVal) {
+    return stackVal
+        ? impureTransitionSrc(transitionName, nextState, stackVal)
+        : pureTransitionSrc(transitionName, nextState);
+
+}
+
+function serialize(initial, strictActions, transitions) {
+    let serialized = `
+function BuildError(msg) {
+    throw \`FSM build error: \${msg}\`;
+}
+BuildError.prototype = Error.prototype;
+
+const initial = "${initial}";
+const strictActions = ${strictActions};
+const fsm = {
+    stack: ['Z'], 
+    state: ['${initial}'],
+    consume: ${this.consume.toString()},
+    dispatch: ${this.dispatch.toString()},
+    reset: ${this.reset.toString()},
+    stackIsInitial: ${this.stackIsInitial.toString()},
+    transitions: {
+`;
+    for (const [name, ruleTransitions] of Object.entries(transitions)) {
+        const transitionSrc = [];
+        for (const key in ruleTransitions) {
+            if (!isMetaProperty(key)) {
+                const nextState = ruleTransitions[`@@nextState_${key}`];
+                const stackVal = ruleTransitions[`@@stackVal_${key}`];
+                transitionSrc.push(makeTransitionSrc(key, nextState, stackVal));
+            }
+        }
+        serialized += `${name}: { ${transitionSrc.join(", ")} },\n`;
+    }
+
+    serialized += "}\n};"
+    serialized += "\nmodule.exports = fsm;\n";
+
+    return serialized;
+}
+module.exports = { serialize };
